9e67d14cf5af22a34d5184c9ea5186d3b1bace22
lukonix <52760438+Lukonix12@users.noreply.github.com>
2024-08-08 09:07:55 -0300

    Add files via upload

--- fase3.js
+++ fase3.js
@@ -10,71 +10,98 @@ const questions = [
 	"Quantas bananas?"
 ];
 
-let n = [
-	Math.floor(Math.random() * 3),
-	Math.floor(Math.random() * 3),
-	Math.floor(Math.random() * 3)
-];
+var uvas = 0, maças = 0, bananas = 0, n = 0, q = 0;
 
-let q = Math.floor(Math.random() * 3);
+function game() {
 
-let uvas = 0, maças = 0, bananas = 0;
+	q = Math.floor(Math.random() * 3);
+	n = [
+		Math.floor(Math.random() * 3),
+		Math.floor(Math.random() * 3),
+		Math.floor(Math.random() * 3)
+	];
 
-document.getElementById("img1").innerHTML = '<img src="'+images[n[0]]+'" width="120px" height="120px"/>';
+	document.getElementById("img1").innerHTML = '<img src="'+images[n[0]]+'" width="120px" height="120px"/>';
+	document.getElementById("img2").innerHTML = '<img src="'+images[n[1]]+'" width="120px" height="120px"/>';
+	document.getElementById("img3").innerHTML = '<img src="'+images[n[2]]+'" width="120px" height="120px"/>';
 
-document.getElementById("img2").innerHTML = '<img src="'+images[n[1]]+'" width="120px" height="120px"/>';
-
-document.getElementById("img3").innerHTML = '<img src="'+images[n[2]]+'" width="120px" height="120px"/>';
-
-if(q == 0){
-	alert(questions[0])
-	for(let i = 0; i < 3; i++){
-		if(n[i] == 0){
-			uvas++;
+	if(q == 0){
+		alert(questions[0])
+		for(let i = 0; i < 3; i++){
+			if(n[i] == 0){
+				uvas++;
+			}
 		}
+		console.log("Uvas: " + uvas)
 	}
-	console.log("Uvas: " + uvas)
-}
-else if(q == 1){
-	alert(questions[1])
-	for(let i = 0; i < 3; i++){
-		if(n[i] == 1){
-			maças++;
+	else if(q == 1){
+		alert(questions[1])
+		for(let i = 0; i < 3; i++){
+			if(n[i] == 1){
+				maças++;
+			}
 		}
+		console.log("Maçãs: " + maças)
 	}
-	console.log("Maçãs: " + maças)
-}
-else{
-	alert(questions[2])
-	for(let i = 0; i < 3; i++){
-		if(n[i] == 2){
-			bananas++;
+	else{
+		alert(questions[2])
+		for(let i = 0; i < 3; i++){
+			if(n[i] == 2){
+				bananas++;
+			}
 		}
+		console.log("Bananas: " + bananas)
 	}
-	console.log("Bananas: " + bananas)
 }
+
+function reset() {
+	uvas = 0;
+	maças = 0;
+	bananas = 0;
+
+	
+}
+
+function ativarResposta() {
+	document.getElementsByClassName("btn_resposta").disabled = false;
+}
+
+game();
 
 function obterResposta(n) {
 	if(q == 0){
 		if(n == uvas){
+			reset();
 			alert("Acertou!");
+			game();
 		}
 		else{
+			reset();
 			alert("Errou!");
+			game();
 		}
 	}
 	else if(q == 1){
 		if(n == maças){
+			reset();
 			alert("Acertou!")
+			game();
 		}else{
+			reset();
 			alert("Errou")
+			game();
 		}
 	}else{
 		if(n == bananas){
+			reset();
 			alert("Acertou!")
+			game();
 		}else{
+			reset();
 			alert("Errou")
+			game();
 		}
 	}
 }
+
 
